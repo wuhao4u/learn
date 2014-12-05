87dd8d4a9e498c896d035013fcc81802233a3095
Hao Wu <wuhao4u@gmail.com>
2014-12-05 14:04:42 -0500

    hidden features #1

---
+++ ArgumentUnpacking.py
@@ -0,0 +1,11 @@
+def draw_point(x, y):
+    # do some magic
+    print(x)
+    print(y)
+
+point_foo = (3, 4)
+point_bar = {'y': 3, 'x': 2}
+
+draw_point(*point_foo)
+draw_point(*point_bar)
+draw_point(**point_bar)
